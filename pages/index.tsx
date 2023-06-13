a5decd4bfb69ff15ed92f04c08278b74b1d00731
yuk6ra <yuki.imamura.all@gmail.com>
2023-06-13 18:32:32 +0900

    Update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,48 @@
-import { ConnectWallet, Web3Button, useAddress } from "@thirdweb-dev/react";
+import { ConnectWallet, Web3Button, useAddress, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { ERC721_ADDRESS } from "../const/constants";
+import NFTGrid from "../components/NFTGrid";
 
 const Home: NextPage = () => {
 
-  const address = useAddress();
+    const address = useAddress();
 
+    const { contract } = useContract(ERC721_ADDRESS);
 
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
+    const { 
+        data,
+        isLoading,
+    } = useOwnedNFTs(contract, address);
 
-        <ConnectWallet />
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
 
-        {!address ? (
-          <p className="style.description">Connect your wallet to claim your NFT</p>
-        ) : (
-          <>
-          <h3>NFT</h3>
+                <ConnectWallet />
 
-          <NFTGrid />
+                {!address ? (
+                    <p className="style.description">Connect your wallet to claim your NFT</p>
+                ) : (
+                    <>
+                        <h3>NFT</h3>
 
-            <Web3Button
-              contractAddress={ERC721_ADDRESS}
-              action={(contract) => contract.erc721.claim(1)}
-            >Claim NFT</Web3Button>
-          </>
-        )}
+                        <NFTGrid
+                            isLoading={isLoading}
+                            nfts={data}
+                            emptyText="No NFTs found"
+                        />
 
-      </main>
-    </div>
-  );
+                        <Web3Button
+                            contractAddress={ERC721_ADDRESS}
+                            action={async (contract) => await contract.erc721.claim(1)}
+                        >Claim NFT</Web3Button>
+                    </>
+                )}
+
+            </main>
+        </div>
+    );
 };
 
 export default Home;
